d5462ec09b099a426419a68185f67842e63e5e4d
Angelbyte <roberto.angel96@live.com>
2025-06-25 21:22:15 -0600

    Ajustar el padding en los componentes Footer y Nav para una mejor presentación.

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
 	return (
 		<footer>
-			<div className="border-b border-gray-200/50 bg-white/70 px-1 py-1.5 text-center text-black shadow-sm backdrop-blur-xl backdrop-saturate-150 md:px-2 md:py-3 dark:border-white/5 dark:bg-black/40 dark:text-white">
+			<div className="border-b border-gray-200/50 bg-white/70 px-1 py-1.5 text-center text-black shadow-sm backdrop-blur-xl backdrop-saturate-150 md:px-2 md:py-2 dark:border-white/5 dark:bg-black/40 dark:text-white">
 				<p className="md:text-xl">Hecho con ♥️ por Angelbyte</p>
 			</div>
 		</footer>
--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -2,7 +2,7 @@ import { ThemeToggle } from '@/components/ThemeToggle'
 
 const Nav = () => {
 	return (
-		<nav className="sticky top-0 z-10 flex w-full justify-between border-b border-gray-200/50 bg-white/70 px-1 py-1.5 text-black shadow-sm backdrop-blur-xl backdrop-saturate-150 md:px-2 md:py-3 dark:border-white/5 dark:bg-black/40 dark:text-white">
+		<nav className="sticky top-0 z-10 flex w-full justify-between border-b border-gray-200/50 bg-white/70 px-1 py-1.5 text-black shadow-sm backdrop-blur-xl backdrop-saturate-150 md:px-2 md:py-2 dark:border-white/5 dark:bg-black/40 dark:text-white">
 			<div className="w-5"></div>
 			<a href="/" className="font-bold uppercase md:text-2xl">
 				SpendList
