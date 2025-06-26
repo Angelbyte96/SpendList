e0837f6f6c7c3034d8a1339d5cca2c72f70545dc
Angelbyte <roberto.angel96@live.com>
2025-06-25 18:43:47 -0600

    Mejorar el diseño y la estructura de los componentes Footer, Nav y Welcome, ajustando estilos y contenido para una mejor presentación.

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -2,10 +2,10 @@ import { ThemeToggle } from '@/components/ThemeToggle'
 
 const Nav = () => {
 	return (
-		<nav className="sticky top-0 z-10 flex w-full justify-between border-b border-gray-200/50 bg-white/70 px-2 py-3 text-black shadow-sm backdrop-blur-xl backdrop-saturate-150 dark:border-white/5 dark:bg-black/40 dark:text-white">
+		<nav className="sticky top-0 z-10 flex w-full justify-between border-b border-gray-200/50 bg-white/70 px-1 py-1.5 text-black shadow-sm backdrop-blur-xl backdrop-saturate-150 md:px-2 md:py-3 dark:border-white/5 dark:bg-black/40 dark:text-white">
 			<div className="w-5"></div>
-			<a href="/" className="uppercase md:text-2xl">
-				Shop List
+			<a href="/" className="font-bold uppercase md:text-2xl">
+				SpendList
 			</a>
 			<ThemeToggle />
 		</nav>
