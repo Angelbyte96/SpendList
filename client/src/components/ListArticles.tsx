aaa16955b89e6d0b4b279535707a855fcdc47a2c
Angelbyte <roberto.angel96@live.com>
2025-07-10 21:10:16 -0600

    feat: integrar lógica para agregar artículos y mostrar lista en NewListForm

--- client/src/components/ListArticles.tsx
+++ client/src/components/ListArticles.tsx
@@ -1,28 +1,29 @@
+import type { List } from '@/lib/localStorageService'
 import { SquarePen, Trash2 } from 'lucide-react'
-import { useState } from 'react'
 
-const ListArticles = () => {
-	const [articles, setArticles] = useState([])
+const ListArticles = ({ listArticles }: { listArticles: Omit<List, 'id' | 'createdAt'> }) => {
+	console.log(listArticles)
 
 	return (
 		<>
-			{articles.length > 0 ? (
+			{listArticles.items.length <= 0 ? (
 				<div>
 					<p className="text-center">No hay articulos por mostrar.</p>
 				</div>
 			) : (
 				<>
-					{/* <p className="text-center">No hay articulos por mostrar.</p> */}
-					<article className="mx-2 flex items-center gap-2 rounded-lg border border-gray-200 p-2">
-						<div className="grow">Pan</div>
-						<div>$35.00</div>
-						<button className="rounded-md border bg-[#3d036622] p-1 text-white dark:border-[#393939]">
-							<SquarePen size={16} />
-						</button>
-						<button className="rounded-md border bg-[#3d036622] p-1 text-white dark:border-[#393939]">
-							<Trash2 size={16} className="text-red-500" />
-						</button>
-					</article>
+					{listArticles.items.map((item) => (
+						<article className="mx-2 flex items-center gap-2 rounded-lg border border-gray-200 p-2">
+							<div className="grow">{item.name}</div>
+							<div>${item.price}</div>
+							<button className="rounded-md border bg-[#3d036622] p-1 text-white dark:border-[#393939]">
+								<SquarePen size={16} />
+							</button>
+							<button className="rounded-md border bg-[#3d036622] p-1 text-white dark:border-[#393939]">
+								<Trash2 size={16} className="text-red-500" />
+							</button>
+						</article>
+					))}
 				</>
 			)}
 		</>
--- client/src/components/NewListForm.tsx
+++ client/src/components/NewListForm.tsx
@@ -1,7 +1,52 @@
 import { ListArticles } from '@/components/ListArticles'
+import type { Item, List } from '@/lib/localStorageService'
 import { MoveLeft, Plus } from 'lucide-react'
+import { useState } from 'react'
 
 const NewListForm = () => {
+	const [list, setList] = useState<Omit<List, 'id' | 'createdAt'>>({
+		name: '',
+		items: [],
+		total: 0,
+	})
+
+	const [currentItem, setCurrentItem] = useState<Item>({ id: '', name: '', price: 0 })
+
+	// Función para crear una nueva lista
+	const createList = () => {
+		const newList: Omit<List, 'id' | 'createdAt'> = {
+			name: list.name,
+			items: list.items,
+			total: list.total,
+		}
+	}
+
+	// Función para agregar un artículo a la lista
+	const addItem = () => {
+		// Validar que el nombre y el precio del artículo sean válidos
+		if (!currentItem.name || currentItem.price <= 0) {
+			alert('Por favor, ingresa un nombre y un precio válido para el artículo.')
+			return
+		}
+
+		// Crear un nuevo artículo con un ID único
+		const newItem: Item = {
+			id: crypto.randomUUID(),
+			name: currentItem.name,
+			price: currentItem.price,
+		}
+
+		// Agregar el nuevo artículo a la lista
+		setList((prevList) => ({
+			...prevList,
+			items: [...prevList.items, newItem],
+			total: prevList.total + newItem.price,
+		}))
+
+		// Limpiar el campo del artículo actual
+		setCurrentItem({ id: '', name: '', price: 0 })
+	}
+
 	return (
 		<section className="grid min-h-full w-full grid-rows-[auto_1fr_auto] border md:gap-8">
 			<header className="m-2 flex items-center justify-start gap-4">
@@ -18,7 +63,7 @@ const NewListForm = () => {
 				<form action="">
 					<div className="m-2 flex flex-col gap-1 rounded-xl border p-2 md:gap-2 md:p-4">
 						<label htmlFor="nameList" className="font-semibold">
-							Nombre de la lista
+							{list.name ? 'Editar Lista' : 'Nombre de la Lista'}
 						</label>
 						<input
 							type="text"
@@ -26,6 +71,8 @@ const NewListForm = () => {
 							id="nameList"
 							placeholder="Ej. Compras del fin de semana"
 							className="rounded-lg border p-1.5"
+							value={list.name}
+							onChange={(e) => setList({ ...list, name: e.target.value })}
 						/>
 					</div>
 					<div className="m-2 flex flex-col gap-1 rounded-xl border p-2 md:gap-2 md:p-4">
@@ -39,14 +86,27 @@ const NewListForm = () => {
 								id="nameArticle"
 								placeholder="Nombre del articulo"
 								className="w-full rounded-lg border p-1.5 sm:flex-grow"
+								value={currentItem.name}
+								onChange={(e) => setCurrentItem({ ...currentItem, name: e.target.value })}
 							/>
 							<input
 								type="number"
 								name="price"
 								placeholder="Precio"
 								className="w-full rounded-xl border p-1.5 sm:w-24"
+								value={currentItem.price || ''}
+								onChange={(e) =>
+									setCurrentItem({
+										...currentItem,
+										price: e.target.value ? parseFloat(e.target.value) : 0,
+									})
+								}
 							/>
-							<button className="flex w-full items-center justify-center gap-2 rounded-lg bg-blue-800 px-3 py-1.5 text-white transition-colors hover:bg-blue-900 sm:w-auto">
+							<button
+								className="flex w-full items-center justify-center gap-2 rounded-lg bg-blue-800 px-3 py-1.5 text-white transition-colors hover:bg-blue-900 sm:w-auto"
+								type="button"
+								onClick={addItem}
+							>
 								<Plus size={18} />
 								<span>Agregar</span>
 							</button>
@@ -54,13 +114,13 @@ const NewListForm = () => {
 					</div>
 				</form>
 				<div>
-					<ListArticles />
+					<ListArticles listArticles={list} />
 				</div>
 			</main>
 			<div className="flex items-center justify-between bg-[#0f172b3d] px-4 py-2">
 				<div>
 					<p>
-						Total: <span>$0.00</span>
+						Total: <span>${list.total}</span>
 					</p>
 				</div>
 				<button className="rounded-lg bg-green-800 px-4 py-2 text-white transition-colors hover:bg-green-900">
