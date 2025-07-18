a60156780f8cdfb29fd1a24ed79b522f27addd9f
Angelbyte <roberto.angel96@live.com>
2025-07-17 20:53:32 -0600

    feat: ajustar el estilo de los botones de lista para mejorar la consistencia visual

--- client/src/components/NewListButton.tsx
+++ client/src/components/NewListButton.tsx
@@ -7,12 +7,12 @@ interface NewListButtonProps {
 const NewListButton = ({ setView }: NewListButtonProps) => {
 	return (
 		<section
-			className="flex cursor-pointer flex-col gap-2 rounded-lg border p-3 dark:bg-[#3c10297d] dark:hover:bg-[#501637] md:w-3/12 w-10/12"
+			className="flex w-10/12 cursor-pointer flex-col gap-2 rounded-lg border p-2 md:w-3/12 dark:bg-[#3c10297d] dark:hover:bg-[#501637]"
 			onClick={() => setView('newList')}
 		>
 			<div className="flex items-center gap-2">
 				<Plus size={22} strokeWidth={3} />
-				<h2 className="text-3xl font-semibold">Nueva Lista</h2>
+				<h2 className="text-2xl font-semibold">Nueva Lista</h2>
 			</div>
 			<div className="">
 				<p className="text-lg">Crear nueva lista de compras</p>
