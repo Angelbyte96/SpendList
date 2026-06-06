a116fa901ae6b2df40c37da0bc264cea132c52f8
Angelbyte96 <108975993+Angelbyte96@users.noreply.github.com>
2026-06-06 16:14:11 +0000

    fix: corregir formato de importaciones en astro.config.mjs

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
-import react from '@astrojs/react'
-import { defineConfig } from 'astro/config'
+import react from '@astrojs/react';
+import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
-import db from '@astrojs/db'
+import db from '@astrojs/db';
 
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.SITE_URL || 'http://localhost:4321',
   output: 'server',
   integrations: [react(), db()],
 
